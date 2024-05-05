d8e4575a0c66503238fb517bb7f935a05c571f72
dongkyunKim <kyun010713@gmail.com>
2024-05-06 01:57:16 +0900

    fix : 테스트 코드 수정
    - CoreMatchers의 equalTo 사용해보기

--- test/chpater1_2/ScoreCollectionTest.java
+++ test/chpater1_2/ScoreCollectionTest.java
@@ -1,13 +1,22 @@
 package chpater1_2;
 
 import static org.junit.Assert.*;
-
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 public class ScoreCollectionTest {
 
     @Test
-    public void test() {
-        
+    public void answerArithmeticMeanOfTwoNumbers() {
+        // 준비
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(() -> 5);
+        collection.add(() -> 7);
+
+        // 실행
+        int actualResult = collection.arithmeticMean();
+
+        // 단언
+        assertThat(actualResult, equalTo(6));
     }
 }
