e15300c8bf6507c048d574d4673710fb3f9a8ead
dongkyunKim <kyun010713@gmail.com>
2024-05-10 00:48:27 +0900

    fix(): Profile

--- src/chpater2/Profile.java
+++ src/chpater2/Profile.java
@@ -4,6 +4,18 @@ import java.util.*;
 
 public class Profile {
     private Map<String, Answer> answers = new HashMap<>();
+    private int score;
+    private String name;
 
+    public String getName() {
+        return name;
+    }
 
+    public Profile(String name) {
+        this.name = name;
+    }
+
+    public void add(Answer answer) {
+        answers.put(answer.get)
+    }
 }
